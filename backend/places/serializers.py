e45e5e4aa00e073ca9291b2a5eceefea8333c9ec
BereketDan <bereketdaniel25@gmail.com>
2023-09-23 14:45:24 -0700

    comment serialize

--- backend/places/serializers.py
+++ backend/places/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+
 class VisitablePlaceSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=255)
     description = serializers.TextField()
@@ -16,3 +17,11 @@ class PlacePostsSerializer(serializers.ModelSerializer):
     photo = serializers.ImageField(upload_to=f'places/posts/photos', null=True, blank=True)
     video = serializers.FileField(upload_to='places/posts/videos', null=True, blank=True)
     
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.CustomUser()
+    text =  serializers.TextField()
+    photo = serializers.ImageField(upload_to=f'places/posts/photos', null=True, blank=True)
+    commented_user = serializers.DateTimeField(auto_now_add=True)
+
