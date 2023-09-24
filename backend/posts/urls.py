270bbb3dcc8a56cd2e4711374a5b78c72c3bce91
Abdi <inventorabdi@gmail.com>
2023-09-24 02:13:52 -0700

    endpoints implemented

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.GetPostView.as_view())
+    path('get_user_posts/<int:user_id>/',views.GetPostView.as_view(), name="user_posts"),
+    path('create_post/', views.GetPostView.as_view(), name='create_post'),
+    path('post/<int:id>/', views.GetPostView.as_view(), name="user_posts"),
 ]
 
