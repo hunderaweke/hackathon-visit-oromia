02a54eff846466566bf811e34fdd12d28bc969db
Abdi <inventorabdi@gmail.com>
2023-09-24 00:57:34 -0700

    urls.py added

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class UserRegister(APIView):
+    def get(request):
+        return Response({'data':'test'})
+    
+
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class GetPostView(APIView):
+    def get(request):
+        return Response({'data':'test'})
