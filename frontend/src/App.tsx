d4d59d8a82f917b791799bd0188231a19dbc5c5f
hunderaweke <hunderaweke@gmail.com>
2023-09-24 11:56:27 +0300

    added:font-icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import { Layout } from './pages/Layout'
+import "./App.css";
+import { Layout } from "./pages/Layout";
 import Home from "./pages/Home";
 import Login from "./components/Login_Signup/Login";
 import Signup from "./components/Login_Signup/Signup";
 
 function App() {
-
   return (
     <>
-     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,11 +1,12 @@
-import { Outlet } from "react-router"
-import { Header } from "../components/1_header/Header"
-
+import { Outlet } from "react-router";
+import { Header } from "../components/1_header/Header";
+import Footer from "../components/10_footer/Footer";
 export const Layout = () => {
   return (
     <>
-        <Header />
-        <Outlet />
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
