af8d1d42f24fb3a5c5f2feb3ca895c3a577ee1e9
kiya kebe <kiyakebe799@gmail.com>
2023-09-24 11:33:09 +0300

    created login and signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import { Layout } from './pages/Layout'
+import Home from "./pages/Home";
+import Login from "./components/Login_Signup/Login";
+import Signup from "./components/Login_Signup/Signup";
 
 function App() {
 
   return (
     <>
-      <Layout />
+     <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
+    </Router>
     </>
   )
 }
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router"
 import { Header } from "../components/1_header/Header"
 
 export const Layout = () => {
   return (
     <>
         <Header />
+        <Outlet />
     </>
   )
 }
