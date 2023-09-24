a3bba5a433bfbb25770c1442ee49fc2d19c818e4
Abdi <inventorabdi@gmail.com>
2023-09-24 02:50:54 -0700

    admin site added

--- backend/visit_oromia/settings.py
+++ backend/visit_oromia/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'places',
     'posts',
     'rest_framework',
+    'rest_framework_simplejwt',
 
     
 ]
@@ -80,15 +81,11 @@ WSGI_APPLICATION = 'visit_oromia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'VisitOromiaDB',
-        'USER':'root',
-        'PASSWORD':'admin',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
