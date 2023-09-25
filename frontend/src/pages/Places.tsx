85d840e8dfe13ce7c68cd191789f0d9629b87503
kiya kebe <kiyakebe799@gmail.com>
2023-09-25 15:07:46 +0300

    updated: map

--- frontend/src/pages/Places.tsx
+++ frontend/src/pages/Places.tsx
@@ -1,35 +1,25 @@
 import PLaceCard from "../components/Places/PlaceCard/PLaceCard";
-// import place from "../../public/images/destination_1.jpg";
 import axios from "axios";
-import { useState,useEffect } from "react";
+import { MapContainer, TileLayer, useMap, Popup, Marker } from "react-leaflet";
+import { useState, useEffect } from "react";
+
 const Places = () => {
-  const [place,setPlace] = useState('');
-  useEffect(()=>{try{
-      axios.get("http://192.168.60.185:5000/places/get_tourist_sites/")
-      .then((res)=>{
-        console.log(res.data)
-      })
-    }catch(error){
-      console.log(error)
-    }},[])
+  // const [place,setPlace] = useState('');
+  // useEffect(()=>{try{
+  //     axios.get("http://192.168.60.185:5000/places/get_tourist_sites/")
+  //     .then((res)=>{
+  //       console.log(res.data)
+  //     })
+  //   }catch(error){
+  //     console.log(error)
+  //   }},[])
   return (
     <>
-      <div className="container py-5 my-5">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1472.2932468631773!2d38.78300532477228!3d8.
-        990596342941773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b852daab62dc5%3A0x42881f1e7f1fe151!2sOromia%20Tourism%20Commission!5e0!3m2!1sen!2set!4v1695577888989!5m2!1sen!2set"
-          width="100%"
-          height="450"
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-
-      </div>
       <div className="container">
         <div className="d-flex flex-wrap justify-content-center">
           <PLaceCard
             title="Bale Mountains"
-            img={'sjhfsjfkdsf'}
+            img={"sjhfsjfkdsf"}
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
               praesentium."
             distance="5Km"
@@ -37,7 +27,7 @@ const Places = () => {
           />
           <PLaceCard
             title="Bale Mountains"
-            img={'sjhfsjfkdsf'}
+            img={"sjhfsjfkdsf"}
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
               praesentium."
             distance="5Km"
@@ -45,7 +35,7 @@ const Places = () => {
           />
           <PLaceCard
             title="Bale Mountains"
-            img={'sjhfsjfkdsf'}
+            img={"sjhfsjfkdsf"}
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
               praesentium."
             distance="5Km"
@@ -53,7 +43,7 @@ const Places = () => {
           />
           <PLaceCard
             title="Bale Mountains"
-            img={'sjhfsjfkdsf'}
+            img={"sjhfsjfkdsf"}
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
               praesentium."
             distance="5Km"
@@ -61,7 +51,7 @@ const Places = () => {
           />
           <PLaceCard
             title="Bale Mountains"
-            img={'sjhfsjfkdsf'}
+            img={"sjhfsjfkdsf"}
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
               praesentium."
             distance="5Km"
@@ -69,7 +59,7 @@ const Places = () => {
           />
           <PLaceCard
             title="Bale Mountains"
-            img={'sjhfsjfkdsf'}
+            img={"sjhfsjfkdsf"}
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
               praesentium."
             distance="5Km"
@@ -77,7 +67,7 @@ const Places = () => {
           />
           <PLaceCard
             title="Bale Mountains"
-            img={'sjhfsjfkdsf'}
+            img={"sjhfsjfkdsf"}
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
               praesentium."
             distance="5Km"
@@ -85,7 +75,7 @@ const Places = () => {
           />
           <PLaceCard
             title="Bale Mountains"
-            img={'sjhfsjfkdsf'}
+            img={"sjhfsjfkdsf"}
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
               praesentium."
             distance="5Km"
