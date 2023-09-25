e8a0923f63167d0c639ab02179a1b77445eee1b8
hunderaweke <hunderaweke@gmail.com>
2023-09-25 09:47:19 +0300

    updated: sigup

--- frontend/src/components/login_signup/Signup.tsx
+++ frontend/src/components/login_signup/Signup.tsx
@@ -2,7 +2,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import logo from "../../assets/logo.png";
-import axios from "axios";
+// import axios from "axios";
 const schema = z
   .object({
     first_name: z
