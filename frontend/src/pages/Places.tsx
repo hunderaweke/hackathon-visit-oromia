ad453e6593a18fb64ed9edaaaee2bf6b2c6581fa
hunderaweke <hunderaweke@gmail.com>
2023-09-26 03:22:28 +0300

    updated:maps

--- frontend/src/pages/Places.tsx
+++ frontend/src/pages/Places.tsx
@@ -8,9 +8,11 @@ const Places = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://192.168.60.185:5000/places/get_tourist_sites/")
+        // .get("http://192.168.60.185:5000/places/get_tourist_sites/")
+        .get("http://127.0.0.1:8000/places/get_tourist_sites/")
         .then((res) => {
           setPlaces(res.data.results);
+          console.log(places);
         });
     } catch (error) {
       console.log(error);
@@ -35,7 +37,16 @@ const Places = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[40, 40]} draggable={true} />
+        <Marker position={[40, 40]} draggable={true}>
+          <Popup>
+            Popup Here
+            <img
+              src={`https://visitoromia.org/wp-content/uploads/2023/06/photo_2021-04-21_13-31-40-1.jpg`}
+              style={{ width: "5rem" }}
+              className="rounded-2"
+            />
+          </Popup>
+        </Marker>
         <Marker position={[60, 40]} />
         <Marker position={[60, 60]} />
         <Marker position={[60, 90]} />
@@ -49,7 +60,7 @@ const Places = () => {
               img={"sjhfsjfkdsf"}
               description={data.description}
               distance={data.latitude}
-              url="/'sjhfsjfkdsf'/detail"
+              url={`/${data.id}/detail`}
             />
           ))}
         </div>
